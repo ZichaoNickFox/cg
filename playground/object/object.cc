a46d0d2aecef66cedbcda7d72d1e275dd8de8462
liuzichao02 <liuzichao02@meituan.com>
2022-04-16 15:44:08 +0800

    [Engine] Optimize Intersect coordinate convert code

--- playground/object/object.cc
+++ playground/object/object.cc
@@ -7,8 +7,7 @@ bool Object::Intersect(Context* context, const glm::vec3& origin_ws, const glm::
   glm::mat4 model_matrix = transform().GetModelMatrix();
   glm::mat4 inverse_model = glm::inverse(model_matrix);
   glm::vec3 origin_ls = inverse_model * glm::vec4(origin_ws, 1.0);
-  glm::vec3 direction_ls = inverse_model * glm::vec4(origin_ws + direction_ws, 1.0) - glm::vec4(origin_ls,1.0);
-
+  glm::vec3 direction_ls = inverse_model * glm::vec4(direction_ws, 0.0);
   glm::vec3 vertex0_ls, vertex1_ls, vertex2_ls, normal_ls, position_ls;
   float distance_ls;
   if (!CGCHECK_NOTNULL(mesh(context))->Intersect(origin_ls, direction_ls, &position_ls, &normal_ls, &distance_ls,
@@ -16,7 +15,7 @@ bool Object::Intersect(Context* context, const glm::vec3& origin_ws, const glm::
     return false;
   }
   result->position_ws = model_matrix * glm::vec4(position_ls, 1.0);
-  result->normal_ws = model_matrix * glm::vec4(position_ls + normal_ls, 1.0) - glm::vec4(result->position_ws, 1.0);
+  result->normal_ws = model_matrix * glm::vec4(normal_ls, 0.0);
   result->distance_ws = distance_ls;
   result->vertex0_ws = model_matrix * glm::vec4(vertex0_ls, 1.0);
   result->vertex1_ws = model_matrix * glm::vec4(vertex1_ls, 1.0);
